9a8372656c4ac5794963d8d0da74905352bae3bc
Luis Canada <canadaluis5@gmail.com>
2021-07-28 19:51:58 -0400

    refactor: Split ps3 func's into gameutil
    Move variables:
     - VOWELS
     - CONSONANTS
     - SCRABBLE_LETTER_VALUES
    
    Move functions:
     - load_words
     - get_frequency_dict
     - get_word_score
     - is_valid_word
     - calculate_handlen

--- ps3.py
+++ ps3.py
@@ -10,95 +10,9 @@
 import math
 import random
 import string
+from gameutil import *
 
-VOWELS = 'aeiou'
-CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
-
-SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
-}
-
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
-
-WORDLIST_FILENAME = "words.txt"
-
-
-def load_words():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
-
-    print("Loading word list from file...")
-    # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r')
-    # wordlist: list of strings
-    wordlist = []
-    for line in inFile:
-        wordlist.append(line.strip().lower())
-    print("  ", len(wordlist), "words loaded.")
-    return wordlist
-
-
-def get_frequency_dict(sequence):
-    """
-    Returns a dictionary where the keys are elements of the sequence
-    and the values are integer counts, for the number of times that
-    an element is repeated in the sequence.
-
-    sequence: string or list
-    return: dictionary
-    """
-
-    # freqs: dictionary (element_type -> int)
-    freq = {}
-    for x in sequence:
-        freq[x] = freq.get(x, 0) + 1
-    return freq
-
-
-# (end of helper code)
-# -----------------------------------
-
-#
-# Problem #1: Scoring a word
-#
-def get_word_score(word, n):
-    """
-    Returns the score for a word. Assumes the word is a
-    valid word.
-
-    You may assume that the input word is always either a string of letters, 
-    or the empty string "". You may not assume that the string will only contain 
-    lowercase letters, so you will have to handle uppercase and mixed case strings 
-    appropriately. 
-
-        The score for a word is the product of two components:
-
-        The first component is the sum of the points for letters in the word.
-        The second component is the larger of:
-            1, or
-            7*wordlen - 3*(n-wordlen), where wordlen is the length of the word
-            and n is the hand length when the word was played
-
-        Letters are scored as in Scrabble; A is worth 1, B is
-        worth 3, C is worth 3, D is worth 2, E is worth 1, and so on.
-
-    word: string
-    n: int >= 0
-    returns: int >= 0
-    """
-
-    pass  # TO DO... Remove this line when you implement this function
-
-#
-# Make sure you understand how this function works and what it does!
-#
 
 
 def display_hand(hand):
@@ -177,44 +91,6 @@ def update_hand(hand, word):
     """
 
     pass  # TO DO... Remove this line when you implement this function
-
-#
-# Problem #3: Test word validity
-#
-
-
-def is_valid_word(word, hand, word_list):
-    """
-    Returns True if word is in the word_list and is entirely
-    composed of letters in the hand. Otherwise, returns False.
-    Does not mutate hand or word_list.
-
-    word: string
-    hand: dictionary (string -> int)
-    word_list: list of lowercase strings
-    returns: boolean
-    """
-
-    pass  # TO DO... Remove this line when you implement this function
-
-#
-# Problem #5: Playing a hand
-#
-
-
-def calculate_handlen(hand):
-    """ 
-    Returns the length (number of letters) in the current hand.
-
-    hand: dictionary (string-> int)
-    returns: integer
-    """
-
-    pass  # TO DO... Remove this line when you implement this function
-
-
-def letter_score(letter):
-    return SCRABBLE_LETTER_VALUES.get(letter, 0)
 
 
 def play_hand(hand, word_list):
