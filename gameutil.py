3ad3a93c1aa93c558706bf73935fe9d48d6c831d
Luis Canada <canadaluis5@gmail.com>
2021-07-29 08:35:03 -0400

    chore: Move letter_score function to top of file

--- gameutil.py
+++ gameutil.py
@@ -9,6 +9,10 @@ SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
+
+def letter_score(letter):
+    return SCRABBLE_LETTER_VALUES.get(letter, 0)
+
 #
 # Problem #1: Scoring a word
 #
@@ -80,10 +84,6 @@ def calculate_handlen(hand):
     """
 
     pass  # TO DO... Remove this line when you implement this function
-
-
-def letter_score(letter):
-    return SCRABBLE_LETTER_VALUES.get(letter, 0)
 
 
 # -----------------------------------
